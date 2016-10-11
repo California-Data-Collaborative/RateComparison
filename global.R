06b68bd76926e848087b05b28bcd7ab564a19121
christophertull <christophertull@users.noreply.github.com>
2016-10-11 14:41:51 -0700

    Hardcoded IRWD residential rates

--- global.R
+++ global.R
@@ -40,18 +40,21 @@ read_data <- function(filename, cust_col, usage_col, month_col, year_col, et_col
 }
 
 is_budget <- switch(utility_code,
+                    "IRWD"=,
                     "MNWD"=,
                     "LVMWD"=,
                     "SMWD"=TRUE,
                     "SMC"=FALSE)
 
 less_than_date <- switch(utility_code,
-                    "MNWD"="2017-01-01",
-                    "LVMWD"="2017-01-01",
-                    "SMWD"="2016-01-01",
-                    "SMC"="2017-01-01")
+                         "IRWD"="2017-01-01",
+                         "MNWD"="2017-01-01",
+                         "LVMWD"="2017-01-01",
+                         "SMWD"="2016-01-01",
+                         "SMC"="2017-01-01")
 
 test_file <- switch(utility_code,
+                    "IRWD"="data/irwd_test.csv",
                     "MNWD"="data/mnwd_test.csv",
                     "LVMWD"="data/lvmwd_test.csv",
                     "SMWD"="data/smwd_test.csv",
@@ -59,44 +62,51 @@ test_file <- switch(utility_code,
 
 #---------------------Utility Specific UI Defaults --------------------------
 default_fixed_charge <- switch(utility_code,
-                       "MNWD"=11.39,
-                       "LVMWD"=18.30,
-                       "SMWD"=8.72,
-                       "SMC"=0)
-
-default_gpcd <- switch(utility_code,
-                               "MNWD"=60,
-                               "LVMWD"=55,
-                               "SMWD"=55,
+                               "IRWD"=10.30,
+                               "MNWD"=11.39,
+                               "LVMWD"=18.30,
+                               "SMWD"=8.72,
                                "SMC"=0)
 
+default_gpcd <- switch(utility_code,
+                       "IRWD"=50,
+                       "MNWD"=60,
+                       "LVMWD"=55,
+                       "SMWD"=55,
+                       "SMC"=0)
+
 default_et_factor <- switch(utility_code,
-                               "MNWD"=0.7,
-                               "LVMWD"=0.8,
-                               "SMWD"=0.8,
-                               "SMC"=0.0)
+                            "IRWD"=0.8,
+                            "MNWD"=0.7,
+                            "LVMWD"=0.8,
+                            "SMWD"=0.8,
+                            "SMC"=0.0)
 
 #******* TIERED RATES ******
 default_tiered_tiers_html <- switch(utility_code,
-                            "MNWD"=,
-                            "LVMWD"='<textarea id="tieredTiers" rows="6" cols="15" style="resize: none;">0\n16\n67\n200</textarea>',
-                            "SMWD"='<textarea id="tieredTiers" rows="6" cols="15" style="resize: none;">0\n7\n21\n36\n71</textarea>',
-                            "SMC"='<textarea id="tieredTiers" rows="6" cols="15" style="resize: none;">0\n15\n41\n149</textarea>')
+                                    "IRWD"=,
+                                    "MNWD"=,
+                                    "LVMWD"='<textarea id="tieredTiers" rows="6" cols="15" style="resize: none;">0\n16\n67\n200</textarea>',
+                                    "SMWD"='<textarea id="tieredTiers" rows="6" cols="15" style="resize: none;">0\n7\n21\n36\n71</textarea>',
+                                    "SMC"='<textarea id="tieredTiers" rows="6" cols="15" style="resize: none;">0\n15\n41\n149</textarea>')
 
 default_tiered_prices_html <- switch(utility_code,
-                            "MNWD"=,
-                            "LVMWD"='<textarea id="tieredPrice" rows="6" cols="15" style="resize: none;">2.31\n2.80\n3.81\n5.34</textarea>',
-                            "SMWD"='<textarea id="tieredPrice" rows="6" cols="15" style="resize: none;">2.04\n2.29\n2.77\n3.28\n4.50</textarea>',
-                            "SMC"='<textarea id="tieredPrice" rows="6" cols="15" style="resize: none;">2.87\n4.29\n6.44\n10.07</textarea>')
+                                     "IRWD"=,
+                                     "MNWD"=,
+                                     "LVMWD"='<textarea id="tieredPrice" rows="6" cols="15" style="resize: none;">2.31\n2.80\n3.81\n5.34</textarea>',
+                                     "SMWD"='<textarea id="tieredPrice" rows="6" cols="15" style="resize: none;">2.04\n2.29\n2.77\n3.28\n4.50</textarea>',
+                                     "SMC"='<textarea id="tieredPrice" rows="6" cols="15" style="resize: none;">2.87\n4.29\n6.44\n10.07</textarea>')
 
 #******* BUDGET RATES ******
 default_budget_tiers_html <- switch(utility_code,
-                                     "MNWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n126%\n151%</textarea>',
-                                     "LVMWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n151%</textarea>',
-                                     "SMWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n151%\n201%</textarea>',
+                                    "IRWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\n40%\n101%\n131%</textarea>',
+                                    "MNWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n126%\n151%</textarea>',
+                                    "LVMWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n151%</textarea>',
+                                    "SMWD"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n151%\n201%</textarea>',
                                     "SMC"='<textarea id="budgetTiers" rows="6" cols="15" style="resize: none;">0\nIndoor\n101%\n151%\n201%</textarea>') 
 
 default_budget_prices_html <- switch(utility_code,
+                                     "IRWD"='<textarea id="budgetPrice" rows="6" cols="15" style="resize: none;">1.11\n1.62\n3.92\n14.53</textarea>',
                                      "MNWD"='<textarea id="budgetPrice" rows="6" cols="15" style="resize: none;">1.49\n1.70\n2.62\n4.38\n9.17</textarea>',
                                      "LVMWD"='<textarea id="budgetPrice" rows="6" cols="15" style="resize: none;">2.36\n3.18\n3.96\n4.98</textarea>',
                                      "SMWD"='<textarea id="budgetPrice" rows="6" cols="15" style="resize: none;">1.86\n2.11\n2.61\n3.21\n4.67</textarea>',
