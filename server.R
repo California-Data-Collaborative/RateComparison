a79c07537cd9d00537322e1a169e0b6c3973da8e
christophertull <christophertull@users.noreply.github.com>
2016-10-27 12:02:30 -0700

    Fixed bug with histogram and boxplot Issue #22

--- server.R
+++ server.R
@@ -197,17 +197,24 @@ shinyServer(function(input, output, clientData, session) {
   #******************************************************************
   df_change <- reactive({
     start.time <- Sys.time()
-    
-    df_change <- df_plots() %>% group_by(cust_id) %>% 
+      df_change <- df_plots() %>% group_by(cust_id) %>% 
       summarise(total_bill=sum(total_bill, na.rm=TRUE), 
                 baseline_bill=sum(baseline_bill, na.rm=TRUE),
                 hypothetical_usage=sum(hypothetical_usage, na.rm=TRUE), #calculating hypothetical and baseline usages
                 baseline_usage=sum(baseline_usage, na.rm=TRUE)) %>%
       dplyr::select(total_bill, baseline_bill, hypothetical_usage, baseline_usage) %>% 
                                                                 #calucating differences in usage
-      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1) %>%
-      filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE),
-             abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 2.5*sd(changes_in_usage, na.rm=TRUE))
+      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1)
+    
+      if (input$displayType == "Revenue"){
+        
+        df_change <- df_change %>% filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE))
+      }
+      
+      else{
+        df_change <- df_change %>% filter(abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 
+                               2.5*sd(changes_in_usage, na.rm=TRUE))
+      }
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
@@ -227,9 +234,17 @@ shinyServer(function(input, output, clientData, session) {
                 baseline_usage=sum(baseline_usage, na.rm=TRUE)) %>%
       dplyr::select(total_bill, baseline_bill, hypothetical_usage, baseline_usage) %>% 
                                                                 #calucating differences in usage                                                        
-      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1) %>%
-      filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE),
-             abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 2.5*sd(changes_in_usage, na.rm=TRUE))
+      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1)
+    
+      if (input$displayType == "Revenue"){
+        
+        df_change1 <- df_change1 %>% filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE))
+      }
+    
+      else{
+        df_change1 <- df_change1 %>% filter(abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 
+                                          2.5*sd(changes_in_usage, na.rm=TRUE))
+    }
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
@@ -249,9 +264,17 @@ shinyServer(function(input, output, clientData, session) {
                 baseline_usage=sum(baseline_usage, na.rm=TRUE)) %>%
       dplyr::select(total_bill, baseline_bill, hypothetical_usage, baseline_usage) %>%
                                                                 #calucating differences in usage
-      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1) %>%
-      filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE),
-             abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 2.5*sd(changes_in_usage, na.rm=TRUE))
+      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1)
+    
+    if (input$displayType == "Revenue"){
+      
+      df_change2 <- df_change2 %>% filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE))
+    }
+    
+    else{
+      df_change2 <- df_change2 %>% filter(abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 
+                                            2.5*sd(changes_in_usage, na.rm=TRUE))
+    }
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
@@ -271,9 +294,17 @@ shinyServer(function(input, output, clientData, session) {
                 baseline_usage=sum(baseline_usage, na.rm=TRUE)) %>%
       dplyr::select(total_bill, baseline_bill, hypothetical_usage, baseline_usage) %>% 
                                                                 #calucating differences in usage
-      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1) %>%
-      filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE),
-             abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 2.5*sd(changes_in_usage, na.rm=TRUE))
+      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1)
+    
+    if (input$displayType == "Revenue"){
+      
+      df_change3 <- df_change3 %>% filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE))
+    }
+    
+    else{
+      df_change3 <- df_change3 %>% filter(abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 
+                                            2.5*sd(changes_in_usage, na.rm=TRUE))
+    }
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
@@ -294,9 +325,17 @@ shinyServer(function(input, output, clientData, session) {
                 baseline_usage=sum(baseline_usage, na.rm=TRUE)) %>%
       dplyr::select(total_bill, baseline_bill, hypothetical_usage, baseline_usage) %>%
                                                                 #calucating differences in usage
-      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1) %>%
-      filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE),
-             abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 2.5*sd(changes_in_usage, na.rm=TRUE))
+      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1)
+    
+    if (input$displayType == "Revenue"){
+      
+      df_change4 <- df_change4 %>% filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE))
+    }
+    
+    else{
+      df_change4 <- df_change4 %>% filter(abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 
+                                            2.5*sd(changes_in_usage, na.rm=TRUE))
+    }
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
@@ -316,9 +355,17 @@ shinyServer(function(input, output, clientData, session) {
                 baseline_usage=sum(baseline_usage, na.rm=TRUE)) %>%
       dplyr::select(total_bill, baseline_bill, hypothetical_usage, baseline_usage) %>%
                                                                 #calucating differences in usage
-      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1) %>%
-      filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE),
-             abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 2.5*sd(changes_in_usage, na.rm=TRUE))
+      mutate(changes=total_bill-baseline_bill, changes_in_usage=hypothetical_usage-baseline_usage, change_group=1)
+    
+    if (input$displayType == "Revenue"){
+      
+      df_change5 <- df_change5 %>% filter(abs(changes) < mean(changes, na.rm=TRUE) + 2.5*sd(changes, na.rm=TRUE))
+    }
+    
+    else{
+      df_change5 <- df_change5 %>% filter(abs(changes_in_usage) < mean(changes_in_usage, na.rm=TRUE) + 
+                                            2.5*sd(changes_in_usage, na.rm=TRUE))
+    }
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
