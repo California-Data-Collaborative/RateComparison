31e7799f9701ae7571e08add6e0be5d6ea4a9bd5
christophertull <christophertull@users.noreply.github.com>
2016-11-25 14:33:12 -0800

    Module for ui input rows

--- R/rate_inputs.R
+++ R/rate_inputs.R
@@ -1,32 +1,52 @@
 
 
-ratePartInput <- function(id, part_name="", field_or_formula="", depends_col_list, current_selected=NULL){
+ratePartInput <- function(id){
   ns <- NS(id)
-  
-  tagList(
-    fluidRow(
-      column(4, textInput(ns("part_name"), label=NULL, value=part_name ) ),
-      column(4, selectInput(ns("depends_col"), label=NULL, choices=depends_col_list, 
-                            selected=current_selected, multiple=TRUE) ),
-      column(4, textInput(ns("field_or_formula"), label=NULL, value=field_or_formula ) )
-    )
-#     ),
-#     
-#     fluidRow(
-#       conditionalPanel(
-#         condition = "input.depends_col == null",
-#         column(12, textInput(ns("field_or_formula"), label=NULL, value=field_or_formula ) )
-#       ),
-#       conditionalPanel(
-#         condition = "input.depends_col != null",
-#         column(12, textInput(ns("field_or_formula"), label=NULL, value="DLKJLJK" ) )
-#       )
-#         
-#     )
-  )
-  
+  uiOutput(ns("inputBox"))
 }
 
-ratePart <- function(input, output, session){
+ratePart <- function(input, output, session, part_name="", depends_col_list, 
+                     current_selected=NULL, labels=FALSE, field_or_formula=""){
+  
+  output$inputBox <- renderUI({
+    ns <- session$ns
+    
+    if(labels){
+      labelList = c("Name:", "Depends on:", "Value:")
+    }
+    else{
+      labelList = c(NULL, NULL, NULL)
+    }
+    
+    tagList(
+      fluidRow(
+        column(4, textInput(ns("part_name"), label=labelList[1], value=part_name) ),
+        column(4, selectInput(ns("depends_col"), label=labelList[2], choices=depends_col_list, 
+                              selected=current_selected, multiple=TRUE) ),
+        column(4, textAreaInput(ns("field_or_formula"), label=labelList[3], value=field_or_formula,
+                                height=50))
+      ),
+      #     ),
+      #     
+      #     fluidRow(
+      #       conditionalPanel(
+      #         condition = "input.depends_col == null",
+      #         column(12, textInput(ns("field_or_formula"), label=NULL, value=field_or_formula ) )
+      #       ),
+      #       conditionalPanel(
+      #         condition = "input.depends_col != null",
+      #         column(12, textInput(ns("field_or_formula"), label=NULL, value="DLKJLJK" ) )
+      #       )
+      #         
+      #     )
+      
+      tags$style(
+        ".selectize-dropdown, .selectize-input, .selectize-input { 
+        line-height: 29px; 
+        }"
+      )
+    )
+    
+  })
   
 }
