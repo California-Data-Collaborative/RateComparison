c32ec348c26a880dfa6688cafed28a60ad8a360a
christophertull <christophertull@users.noreply.github.com>
2017-01-16 15:54:13 -0800

    Added estimated usage for each class in scenario planning

--- ui.R
+++ ui.R
@@ -20,37 +20,74 @@ shinyUI(navbarPage(
     )#end row
   ),#end tabpanel for navar
   tabPanel("Scenario Planning",
+     fluidRow(
+       column(2, checkboxInput("Planning", "Enable Scenario Planning", value = TRUE, 
+                               width = NULL)),
+       column(3, numericInput("Months", "Number of months to Forecast", 
+                              3, min = 1, max = 24, step = 1,
+                              width = NULL)
+       )
+     ),
+     fluidRow(
+       column(2),
+       column(2, h4("Monthly Growth/Decline\n(# of accounts)")),
+       column(2, h4("Estimated Usage per\nNew Account (ccf)"))
+     ),
+     fluidRow(
+       column(2, strong("Single-Family Residential")),
+       column(2, numericInput("ResidentialSingle", NULL, 
+                              5, min = -1000, max = 1000, step = 1,
+                              width = NULL)),
+       column(2, numericInput("EstUsagePerAccount", NULL,
+                              10, min = 0, max = 1000, step = NA,
+                              width = NULL))
+     ),
+     fluidRow(
+       column(2, strong("Multi-Family Residential")),
+       column(2, numericInput("ResidentialMulti", NULL, 
+                              1, min = -1000, max = 1000, step = 1,
+                              width = NULL)),
+       column(2, numericInput("EstUsagePerAccount_multi", NULL,
+                              10, min = 0, max = 1000, step = NA,
+                              width = NULL))
+     ),
+     fluidRow(
+       column(2, strong("Irrigation")),
+       column(2, numericInput("Irrigation", NULL, 
+                              1, min = -1000, max = 1000, step = 1,
+                              width = NULL)),
+       column(2, numericInput("EstUsagePerAccount_irrigation", NULL,
+                              10, min = 0, max = 1000, step = NA,
+                              width = NULL))
+     ),
+     fluidRow(
+       column(2, strong("Commercial")),
+       column(2, numericInput("Commercial", NULL, 
+                              -1, min = -1000, max = 1000, step = 1,
+                              width = NULL)),
+       column(2, numericInput("EstUsagePerAccount_commercial", NULL,
+                              10, min = 0, max = 1000, step = NA,
+                              width = NULL))
+     ),
+     fluidRow(
+       column(2, strong("Institutional")),
+       column(2, numericInput("Institutional", NULL, 
+                              1, min = -1000, max = 1000, step = 1,
+                              width = NULL)),
+       column(2, numericInput("EstUsagePerAccount_institutional", NULL,
+                              10, min = 0, max = 1000, step = NA,
+                              width = NULL))
+     ),
+     fluidRow(
+       column(2, strong("Other")),
+       column(2, numericInput("Other", NULL, 
+                             1, min = -1000, max = 1000, step = 1,
+                             width = NULL)),
+       column(2, numericInput("EstUsagePerAccount_other", NULL,
+                              10, min = 0, max = 1000, step = NA,
+                              width = NULL))
+     )
      
-     checkboxInput("Planning", "Enable Scenario Planning", value = TRUE, 
-                   width = NULL),
-     
-     numericInput("Months", "Number of months to Forecast", 
-                  3, min = 1, max = 24, step = 1,
-                  width = NULL),
-     
-     numericInput("ResidentialSingle", "Residential Single accounts growth/decline per Month", 
-                  5, min = -1000, max = 1000, step = 1,
-                  width = NULL),
-     numericInput("ResidentialMulti", "Residential Multi accounts growth/decline per Month", 
-                  1, min = -1000, max = 1000, step = 1,
-                  width = NULL),
-     numericInput("Irrigation", "Irrigation accounts growth/decline per Month", 
-                  1, min = -1000, max = 1000, step = 1,
-                  width = NULL),
-     numericInput("Commercial", "Commercial accounts growth/decline per Month", 
-                  -1, min = -1000, max = 1000, step = 1,
-                  width = NULL),
-     numericInput("Other", "Other accounts growth/decline per Month", 
-                  1, min = -1000, max = 1000, step = 1,
-                  width = NULL),
-     numericInput("Institutional", "Institutional accounts growth/decline per Month", 
-                  1, min = -1000, max = 1000, step = 1,
-                  width = NULL),
-     
-     
-     numericInput("EstUsagePerAccount", "Estimated Usage per New Account(ccf)",
-                  10, min = 0, max = 1000, step = NA,
-                  width = NULL)
   )
 
 ))
